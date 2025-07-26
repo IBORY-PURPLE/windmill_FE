76e409a9b4abcffeebf70a6621a58cb55ae87338
채우 <lala1522@gachon.ac.kr>
2025-07-26 16:36:35 +0900

    added home design + log function + transaction

--- frontend/src/api/mystock.jsx
+++ frontend/src/api/mystock.jsx
@@ -38,3 +38,20 @@ export const addMyStock = async (stockData) => {
   if (!res.ok) throw new Error("포지션 추가 실패");
   return res.json();
 };
+
+export const fetchMyStockLogs = async (stockId) => {
+  const token = getAuthToken();
+  const res = await fetch(
+    `https://windmill-be-iqxx.onrender.com/user/mystock/${stockId}`,
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+
+  if (!res.ok) throw new Error("거래 로그 조회 실패");
+  const data = await res.json();
+  console.log(data.data);
+  return data.data; // 배열 형태로 로그가 올 것으로 예상
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px", // 이 지점부터 사이드 패널 표시
+      lg: "1024px",
+    },
   },
   plugins: [],
 };
