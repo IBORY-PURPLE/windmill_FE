79af1015dd1618887038092db92d051238cf8565
채우 <lala1522@gachon.ac.kr>
2025-08-12 18:27:07 +0900

    test saved portfolio

---
+++ frontend/src/assets/dummySavePortfolioList.jsx
@@ -0,0 +1,14 @@
+export const dummySavePortfolioList = [
+  {
+    portfolioId: "a1",
+    name: "첫번째 포트폴리오",
+  },
+  {
+    portfolioId: "a2",
+    name: "두번째 포트폴리오",
+  },
+  {
+    portfolioId: "a3",
+    name: "세번째 포트폴리오",
+  },
+];
